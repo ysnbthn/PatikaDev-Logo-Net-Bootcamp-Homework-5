b8ad838d22263cfdc12ba4ac12b306ea6ce54620
Yasin Batuhan Özyürek <yasinbatuhanozyurek@gmail.com>
2022-04-02 15:39:47 +0300

    Add project files.

---
+++ Homework-5/Program.cs
@@ -0,0 +1,10 @@
+using Homework_5;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
